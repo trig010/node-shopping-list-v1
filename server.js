e7d44d1227dc510bc64e49d8d34e90526840381d
trig010 <trig.code@gmail.com>
2017-02-06 22:27:31 -0600

    recipes GET working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -23,6 +23,14 @@ ShoppingList.create('peppers', 4);
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
+Recipes.create('pbj', ['peanut butter', 'jam', 'bread'])
+
+app.get('/recipes', (req, res) => {
+    res.json(Recipes.get())
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
